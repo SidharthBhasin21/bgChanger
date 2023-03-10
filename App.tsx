d4a255552284c418c09f3614b8c7d9b48a2ea237
Sidharth Bhasin <sid21bhasin@gmail.com>
2023-03-10 12:48:42 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View style = {styles.appContainer}>
+      <Text>This</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  appContainer:{
+    backgroundColor: '#FFF3FF',
+    height:"100%",
+    
+  },
+})
