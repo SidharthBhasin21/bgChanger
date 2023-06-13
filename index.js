54856413bff0557c51d2a1d44c2e48e97769abf4
Yawar <yawarmushtaq52@gmail.com>
2023-06-14 04:36:58 +0530

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:index.js

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ import App from './App';
 import {name as appName} from './app.json';
 
 //This is the code added by sid
-
+// this is added by yawar
 
 AppRegistry.registerComponent(appName, () => App);
