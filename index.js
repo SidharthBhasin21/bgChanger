824319ec47f433be7f3a4ecc24477f3c742518a1
Sidharth Bhasin <sid21bhasin@gmail.com>
2023-06-14 04:43:29 +0530

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+//
+//Assume that sid added few lines of code here
+//
 
 AppRegistry.registerComponent(appName, () => App);
 //yawar added a comment
